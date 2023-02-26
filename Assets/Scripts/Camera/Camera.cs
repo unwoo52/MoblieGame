e3af327c39594e2e711fb2f017e5d6cf0ba89446
Unwoo <73688472+unwoo52@users.noreply.github.com>
2023-02-26 09:57:57 +0900

    .

---
+++ Assets/Scripts/Camera/Camera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraManagement
+{
+    public class Camera : MonoBehaviour
+    {
+        
+        //카메라는 탑뷰, 정해진 
+    }
+
+    public class CameraManagement
+    {
+        float cameraHigh = 1.0f;
+        public float GetCameraHigh()
+        {
+            return cameraHigh;
+        }
+    }
+}
+
